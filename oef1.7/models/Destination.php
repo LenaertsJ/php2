3c333c73f3a547a6f39a569892e40eb331d4f1d8
Julie Lenaerts <julielenaerts@gmail.com>
2021-03-09 10:12:05 +0100

    fix abstract class

--- oef1.7/models/Destination.php
+++ oef1.7/models/Destination.php
@@ -3,11 +3,69 @@
 
 abstract class Destination
 {
-    abstract public function getId();
-    abstract public function getFilename();
-    abstract public function setFilename($filename);
-    abstract public function getTitle();
-    abstract public function setTitle($title);
-    abstract public function toArray();
-    abstract public function toArray2();
+    protected $id;
+    protected $filename;
+    protected $title;
+
+    protected function getId(): int
+    {
+        return $this->id;
+    }
+
+    protected function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    protected function getFilename(): string
+    {
+        return $this->filename;
+    }
+
+    protected function setFilename($filename): void
+    {
+        $this->filename = $filename;
+    }
+
+    /**
+     * @return mixed
+     */
+    protected function getTitle()
+    {
+        return strtoupper( $this->title );
+    }
+
+    /**
+     * @param mixed $title
+     */
+    protected function setTitle($title): void
+    {
+        $this->title = $title;
+    }
+
+    protected function toArray(): array
+    {
+        return [
+            "id" => $this->getId(),
+            "filename" => $this->getFilename(),
+            "title" => $this->getTitle(),
+            "width" => $this->getWidth(),
+            "height" => $this->getHeight(),
+            "published" => $this->getPublished(),
+            "lan_id" => $this->getLanId(),
+            "date" => $this->getDate()
+        ];
+    }
+
+    protected function toArray2(): array
+    {
+        $retarr = [];
+
+        foreach( $this as $key => $value )
+        {
+            $retarr[$key] = $value;
+        }
+        return $retarr;
+    }
+
 }
